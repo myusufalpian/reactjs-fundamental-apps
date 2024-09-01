7b2e3db0538001687d30cc67508e8e75f9f41557
Muhammad Yusuf Alpian <llawliet1945@gmail.com>
2024-09-01 13:10:05 +0700

    add rendering list for render product list

--- src/components/fragments/CardProduct.tsx
+++ src/components/fragments/CardProduct.tsx
@@ -16,7 +16,7 @@ interface Props {
 const CardProduct = (props: Props) => {
     const { children } = props;
     return (
-        <div className="w-full max-w-sm m-8 bg-gray-800 border border-gray-700 rounded-lg shadow">
+        <div className="flex flex-col justify-between w-full max-w-sm m-8 bg-gray-800 border border-gray-700 rounded-lg shadow ">
             {children}
         </div>
     )
@@ -27,7 +27,7 @@ const Header = (props: Props) => {
     const { imgSource } = props;
     return (
         <a href="">
-            <img src={imgSource} alt="Dad Shoes" className="p-5 rounded-t-lg" />
+            <img src={imgSource} alt="Dad Shoes" className="p-8 rounded-t-lg" />
         </a>
     )
 }
@@ -35,9 +35,9 @@ const Header = (props: Props) => {
 const Body = ( props: Props) : JSX.Element => {
     const { tittle, text } = props;
     return (
-        <div>
-            <h5 className="pb-5 mb-2 text-xl font-semibold tracking-tight text-center text-white">{tittle}</h5>
-            <p className="mx-5 mb-5 font-normal text-white">{text}</p>
+        <div className="h-full px-5 pb-5">
+            <h5 className="pb-4 text-xl font-semibold tracking-tight text-white">{tittle}</h5>
+            <p className="font-normal text-white text-s">{text}</p>
         </div>
     )
 }
@@ -45,8 +45,8 @@ const Body = ( props: Props) : JSX.Element => {
 const Footer = (props: Props) => {
     const { btnText, variant = '', onClickHandler, price } = props;
     return (
-        <div className="flex items-center justify-between"> 
-            <span className="py-5 pl-5 text-xl font-bold text-white">{`Rp. ${price?.toLocaleString('id-ID')},00`}</span>
+        <div className="flex items-center justify-between px-5 pb-5"> 
+            <span className="text-xl font-bold text-white">{`Rp. ${price?.toLocaleString('id-ID')},00`}</span>
                 <Button variant = {variant} text='text-white' children={btnText} classname = 'my-5 mr-5' onClick={() => onClickHandler} />
         </div>
     )
--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,26 +1,43 @@
 import CardProduct from "../components/fragments/CardProduct";
 
+const ProductList = [
+    {
+        id: 1,
+        tittle: "Asics Unisex GEL-KAYANO 14 Standard",
+        price: 2199000,
+        imageSource: "https://swift-thumbor.sirclocdn.com/unsafe/960x1120/filters:format(webp):quality(100)/https://bo.asics.co.id/media/catalog/product/cache/5851c493ecee7427253955673b4ab287/1/2/1203a537.020__0003s_0001_2.jpg",
+        description: "Sepatu GEL-KAYANO™ 14 kembali hadir dengan Menginterpretasikan ulang kemampuan performa sepatu dengan bahan dan komponen yang diperbarui, versi ini menjadi iterasi pertama dalam garis keturunan sneaker ini yang dirancang oleh orang lain selain Toshikazu Kayano."
+    },
+    {
+        id: 2,
+        tittle: "ASICS Unisex Gel-Nyc™ Utility Standard",
+        price: 2099000,
+        imageSource: "https://swift-thumbor.sirclocdn.com/unsafe/960x1120/filters:format(webp):quality(100)/https://bo.asics.co.id/media/catalog/product/cache/5851c493ecee7427253955673b4ab287/1/2/1203a383.022__0006s_0000_1.jpg",
+        description: "Sneaker GEL-NYC™ yang bersumber dari inspirasi gaya lari warisan dan modern. Konstruksi bagian atasnya mengacu pada sepatu GEL-NIMBUS™ 3 dari awal tahun 2000-an dan mencampurnya dengan berbagai hiasan dari desain GEL-MC PLUS™ V. Alat bantu tersebut kontras dengan pengaruh retro dibagian atas dengan menggunakan sistem alat bantu sepatu GEL-CUMULUS™ 16."
+    }, 
+    {
+        id: 3,
+        tittle: "ASICS Women Gel-Nimbus 26 Standard",
+        price: 2499000,
+        imageSource: "https://swift-thumbor.sirclocdn.com/unsafe/960x1120/filters:format(webp):quality(100)/https://bo.asics.co.id/media/catalog/product/cache/5851c493ecee7427253955673b4ab287/1/0/1012b601.102__0007s_0003_4.jpg",
+        description: "Rasakan sensasi mendarat di atas awan dengan sepatu GEL-NIMBUS™ 26 yang memiliki bantalan lembut. Desain terbaru ini diciptakan untuk memberikan pengalaman berlari yang lebih lembut dan mulus. Bagian atas dari rajutan lembut dengan teknik khusus membungkus kaki kamu dengan nyaman sekaligus memberikan ventilasi yang optimal. Selain itu, lidah dan kerah rajutan memberikan rasa lembut dan mendukung."
+    }
+]; 
+
+
 const ProductsPage = () => {
     const handlerAddToCart = () => {
         console.log("Add to Cart");
     }
     return (
         <div className="flex justify-center">
-            <CardProduct> 
-                <CardProduct.Header imgSource="https://swift-thumbor.sirclocdn.com/unsafe/960x1120/filters:format(webp):quality(100)/https://bo.asics.co.id/media/catalog/product/cache/5851c493ecee7427253955673b4ab287/1/2/1203a537.020__0003s_0001_2.jpg"/>
-                <CardProduct.Body text="Sepatu GEL-KAYANO™ 14 kembali hadir dengan Menginterpretasikan ulang kemampuan performa sepatu dengan bahan dan komponen yang diperbarui, versi ini menjadi iterasi pertama dalam garis keturunan sneaker ini yang dirancang oleh orang lain selain Toshikazu Kayano." tittle="Asics Unisex GEL-KAYANO 14 Standard"/>
-                <CardProduct.Footer price = {2199000} variant="bg-green-700" children={`Add to Cart`} onClickHandler={() => handlerAddToCart}/>
-            </CardProduct>
-            <CardProduct> 
-                <CardProduct.Header imgSource="https://swift-thumbor.sirclocdn.com/unsafe/960x1120/filters:format(webp):quality(80)/https://bo.asics.co.id/media/catalog/product/cache/5851c493ecee7427253955673b4ab287/1/2/1203a383.022__0006s_0000_1.jpg"/>
-                <CardProduct.Body text="Sneaker GEL-NYC™ yang bersumber dari inspirasi gaya lari warisan dan modern. Konstruksi bagian atasnya mengacu pada sepatu GEL-NIMBUS™ 3 dari awal tahun 2000-an dan mencampurnya dengan berbagai hiasan dari desain GEL-MC PLUS™ V. Alat bantu tersebut kontras dengan pengaruh retro dibagian atas dengan menggunakan sistem alat bantu sepatu GEL-CUMULUS™ 16." tittle="ASICS Unisex Gel-Nyc Utility Standard"/>
-                <CardProduct.Footer price = {2199000} variant="bg-green-700" children={`Add to Cart`} onClickHandler={() => handlerAddToCart}/>
-            </CardProduct>
-            <CardProduct> 
-                <CardProduct.Header imgSource="https://swift-thumbor.sirclocdn.com/unsafe/960x1120/filters:format(webp):quality(80)/https://bo.asics.co.id/media/catalog/product/cache/5851c493ecee7427253955673b4ab287/1/0/1012b601.102__0007s_0003_4.jpg"/>
-                <CardProduct.Body text="Rasakan sensasi mendarat di atas awan dengan sepatu GEL-NIMBUS™ 26 yang memiliki bantalan lembut. Desain terbaru ini diciptakan untuk memberikan pengalaman berlari yang lebih lembut dan mulus. Bagian atas dari rajutan lembut dengan teknik khusus membungkus kaki kamu dengan nyaman sekaligus memberikan ventilasi yang optimal. Selain itu, lidah dan kerah rajutan memberikan rasa lembut dan mendukung." tittle="ASICS Women Gel-Nimbus 26 Standard"/>
-                <CardProduct.Footer price = {2499000} variant="bg-green-700" children={`Add to Cart`} onClickHandler={() => handlerAddToCart}/>
-            </CardProduct>
+            {ProductList.map((product) => (
+                <CardProduct key={product.id}> 
+                    <CardProduct.Header imgSource={product.imageSource}/>
+                    <CardProduct.Body text={product.description} tittle={product.tittle}/>
+                    <CardProduct.Footer price = {product.price} variant="bg-green-700" children={`Add to Cart`} onClickHandler={() => handlerAddToCart}/>
+                </CardProduct>
+            ))}
         </div>
     )
 }
